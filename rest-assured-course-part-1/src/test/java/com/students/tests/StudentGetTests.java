8086faf706de6ad8284f8331b22900a27022466a
VictorPapuc <44552530+VictorPapuc@users.noreply.github.com>
2019-11-13 12:07:04 +0200

    First Commit

---
+++ rest-assured-course-part-1/src/test/java/com/students/tests/StudentGetTests.java
@@ -0,0 +1,29 @@
+package com.students.tests;
+
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+
+
+public class StudentGetTests {
+
+
+    @BeforeClass
+    public static void init() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 8005;
+        RestAssured.basePath = "/student";
+    }
+
+
+    @Test
+    public void getAllStudentsInformation() {
+        Response response = given().when().get("/list");
+        System.out.println(response.body().prettyPrint());
+
+    }
+}
