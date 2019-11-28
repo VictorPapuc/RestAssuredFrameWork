ac7cac21ba5bfac3fca82982c54c9e95881e8bac
VictorPapuc <44552530+VictorPapuc@users.noreply.github.com>
2019-11-28 15:44:53 +0200

    Rebased develop

--- rest-assured-course-part-1/src/test/java/com/catapi/PostInApiCat.java
+++ rest-assured-course-part-1/src/test/java/com/catapi/PostInApiCat.java
@@ -1,8 +1,6 @@
 package com.catapi;
 
 import com.cat.BaseCatApi;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
@@ -13,7 +11,6 @@ public class PostInApiCat {
     public static void main(String[] args) {
 
         ObjectMapper objectMapper = new ObjectMapper();
-
         String location = "/Users/victor.papuc/IdeaProjects/adswizzudemycourse/rest-assured-course-part-1/src/main/resources/files/createvote.json";
 
         try {
