83579863327854794d88121a1b5d24336c00daa2
VictorPapuc <44552530+VictorPapuc@users.noreply.github.com>
2019-11-28 10:59:18 +0200

    Fix for script and added file to gitignore

--- rest-assured-course-part-1/src/main/resources/script.sh
+++ rest-assured-course-part-1/src/main/resources/script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cd /Users/victor.papuc/IdeaProjects/adswizzudemycourse/rest-assured-course-part-1/src/main/resources
-java -jar rest.jar
+cd /Users/${USER}/IdeaProjects/adswizzudemycourse/rest-assured-course-part-1/src/main/resources
+java -jar rest.jar --server.port=8081
